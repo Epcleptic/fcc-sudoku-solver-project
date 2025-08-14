10423d76a8b3b28b2e6490ace4cd829000687320
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2025-08-14 17:07:58 +0200

    validate(puzzleString)

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,25 @@
 class SudokuSolver {
-
+  // The validate function should take a given puzzle string and check it to see if it has 81 valid characters for the input.
   validate(puzzleString) {
+    return !!puzzleString.match(/^[1-9.]{81}$/);
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
-
+  // The check functions should be validating against the current state of the board.
+  getRow(puzzleString, row) {
+    const start =
+      ["A", "B", "C", "D", "E", "F", "G", "H", "I"].indexOf(row.toUpperCase()) *
+      9;
+    const end = start + 9;
+    return puzzleString.slice(start, end);
   }
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  // The solve function should handle solving any given valid puzzle string, not just the test inputs and solutions. You are expected to write out the logic to solve this.
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,29 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  test("Logic handles a valid puzzle string of 81 characters", function () {
+    assert.isTrue(
+      solver.validate(
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+      )
+    );
+  });
+  test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", function () {
+    assert.isFalse(
+      solver.validate(
+        "0.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37."
+      )
+    );
+  });
+  test("Logic handles a puzzle string that is not 81 characters in length", function () {
+    assert.isFalse(
+      solver.validate(
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37"
+      )
+    );
+  });
 });
